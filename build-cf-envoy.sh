aad1978ca8a4af4a3aa2704a54d1d076d9271697
Xavier <xavier@reblaze.com>
2021-04-14 22:32:48 +0200

    fix istio envoy binary: export symbols
    Signed-off-by: Xavier <xavier@reblaze.com>

--- build-cf-envoy.sh
+++ build-cf-envoy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd "${0%/*}" || exit 1
 export PATH=/opt/llvm/bin:$PATH
+export BAZEL_BUILD_ARGS="--define exported_symbols=enabled"
 make build_envoy
 cp bazel-out/k8-opt/bin/src/envoy/envoy cf-out
